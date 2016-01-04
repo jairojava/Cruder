a98039e912a730f83db80fac7a67a7e97342bdcd
jairogithub <darksideonthemoonhotmail.com>
2016-01-04 11:44:41 +0100

    Cambios 11:45

--- app/src/main/java/f/cruder/MainActivity.java
+++ app/src/main/java/f/cruder/MainActivity.java
@@ -1,18 +1,48 @@
 package f.cruder;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView info;
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        info =(TextView)findViewById(R.id.info);
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
 }
